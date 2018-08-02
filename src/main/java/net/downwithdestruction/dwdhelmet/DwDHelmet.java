6cf0bdaa7bcd24a94c3d5ce9032c38d05edf8b8b
MadMac MacAllister <anthonymmacallister@gmail.com>
2018-08-01 23:32:57 -0400

    Updated to 1.13, Paper support only.

--- src/main/java/net/downwithdestruction/dwdhelmet/DwDHelmet.java
+++ src/main/java/net/downwithdestruction/dwdhelmet/DwDHelmet.java
@@ -6,7 +6,7 @@ import net.downwithdestruction.dwdhelmet.listener.PlayerListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class DwDHelmet extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
